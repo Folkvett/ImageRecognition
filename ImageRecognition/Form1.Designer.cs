84ebe1b251de8419a9ff09e3d4c75cdb691ebf67
Patrik Bergström <patbe666@student.liu.se>
2015-05-15 15:08:34 +0200

    First commit, committing everything cuz noob

---
+++ ImageRecognition/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ImageRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.stencilBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stencilBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(296, 285);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(163, 41);
+            this.btnSelectImage.TabIndex = 0;
+            this.btnSelectImage.Text = "Select Image";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(12, 12);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(260, 180);
+            this.imageBox.TabIndex = 1;
+            this.imageBox.TabStop = false;
+            // 
+            // stencilBox
+            // 
+            this.stencilBox.Location = new System.Drawing.Point(411, 12);
+            this.stencilBox.Name = "stencilBox";
+            this.stencilBox.Size = new System.Drawing.Size(260, 180);
+            this.stencilBox.TabIndex = 2;
+            this.stencilBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 338);
+            this.Controls.Add(this.stencilBox);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.btnSelectImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stencilBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.PictureBox stencilBox;
+    }
+}
+
---
+++ ImageRecognition/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageRecognition
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap stencil;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Create a sliding window, a miniature image consisting of only a few pixels within
+        // the full image
+        private void slideWindow(Bitmap image)
+        {
+            stencil = new Bitmap(image);
+            for (int y = 0; y < stencil.Height; ++y)
+                for (int x = 0; x < stencil.Width; ++x)
+                    stencil.SetPixel(x, y, Color.White);
+
+            Color[,] window = new Color[3,3];
+
+            // Get pixels for the 3x3 window, and slide it one pixel at a time
+            for (int y = 0; y < image.Height-3; ++y)
+            {
+                for (int x = 0; x < image.Width-3; ++x)
+                {
+                    window[0, 0] = image.GetPixel(x, y);
+                    window[1, 0] = image.GetPixel(x+1, y);
+                    window[2, 0] = image.GetPixel(x+2, y);
+
+                    window[0, 1] = image.GetPixel(x, y+1);
+                    window[1, 1] = image.GetPixel(x+1, y+1);
+                    window[2, 1] = image.GetPixel(x+1, y+1);
+
+                    window[0, 2] = image.GetPixel(x, y+2);
+                    window[1, 2] = image.GetPixel(x+1, y+2);
+                    window[2, 2] = image.GetPixel(x+2, y+2);
+
+                    // The two current checks, look for difference in brightness in horizontal ...
+                    if(Math.Max(window[0, 0].GetBrightness(), window[2,0].GetBrightness()) -0.1 >
+                        Math.Min(window[0,0].GetBrightness(), window[2, 0].GetBrightness()) &&
+
+                        Math.Max(window[0, 1].GetBrightness(), window[2,1].GetBrightness()) -0.1 >
+                        Math.Min(window[0,1].GetBrightness(), window[2, 1].GetBrightness()) &&
+
+                        Math.Max(window[0, 2].GetBrightness(), window[2,2].GetBrightness()) -0.1 >
+                        Math.Min(window[0,2].GetBrightness(), window[2, 2].GetBrightness()))
+                    {
+                       stencil.SetPixel(x + 1, y + 1, Color.Black);
+                    }
+
+                    // .. and in vertical, if there is a difference, mark the "pivot" black in the stencil.
+                    if (Math.Max(window[0, 0].GetBrightness(), window[0, 2].GetBrightness()) - 0.1 >
+                        Math.Min(window[0, 0].GetBrightness(), window[0, 2].GetBrightness()) &&
+
+                        Math.Max(window[1, 0].GetBrightness(), window[1, 2].GetBrightness()) - 0.1 >
+                        Math.Min(window[1, 0].GetBrightness(), window[1, 2].GetBrightness()) &&
+
+                        Math.Max(window[2, 0].GetBrightness(), window[2, 2].GetBrightness()) - 0.1 >
+                        Math.Min(window[2, 0].GetBrightness(), window[2, 2].GetBrightness()))
+                    {
+                        stencil.SetPixel(x + 1, y + 1, Color.Black);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selectImageDialog = new OpenFileDialog();
+
+            selectImageDialog.InitialDirectory = "c:\\Users\\";
+            selectImageDialog.Filter = "image files (*.bmp, *.jpg, *.png)|*.bmp;*.jpg;*.png";
+
+            if (selectImageDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Create a bitmap image from the selected file
+                    Bitmap selectedImage = (Bitmap)Image.FromFile(selectImageDialog.FileName, true);
+                    
+                    btnSelectImage.Text = selectImageDialog.FileName;
+
+                    // Call on the sliding window function
+                    slideWindow(selectedImage);
+
+                    // Present the results
+                    stencilBox.SizeMode = PictureBoxSizeMode.AutoSize;
+                    stencilBox.Image = stencil;
+
+                    imageBox.SizeMode = PictureBoxSizeMode.AutoSize;
+                    imageBox.Image = selectedImage;                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
